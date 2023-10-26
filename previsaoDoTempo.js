384caefb6ef21d6b127f60ce65fca66c5d335da2
yasbonilha <bonilhayaya@gmail.com>
2023-10-26 11:43:40 -0300

    ap09(js): adição de do npm, gitignore e api

---
+++ previsaoDoTempo.js
@@ -0,0 +1,3 @@
+c5932abdd52f7538f52c04b2ac845738
+
+// não podemos deixar a chave da api por duas formas - segurança e variável de ambiente
--- promiseFullfied.js
+++ promiseFullfied.js
@@ -1,17 +1,30 @@
-const calculoRapidinho = (numero) => {
-    return Promise.resolve(
-        (numero/2) * (numero + 1)
-    )
-}
+// const calculoRapidinho = (numero) => {
+//     return Promise.resolve(
+//         (numero/2) * (numero + 1)
+//     )
+// }
 
-//  essa promise já nasce fullfied.
+// //  essa promise já nasce fullfied.
 
-calculoRapidinho(10).then(function(res) {console.log(res)})
-// toda vez que nós criamos uma promise, temos que dizer depois o que vamos fazer com ela
+// calculoRapidinho(10).then(function(res) {console.log(res)})
+// // toda vez que nós criamos uma promise, temos que dizer depois o que vamos fazer com ela
 
 
 const calcular = (numero) => {
-    // desafio 01
-    // se o usuario informar um numero positivo, fazer como antes
-    
+    // if(numero >=1) {
+    //     return Promise.resolve(
+    //         (numero/2) * (numero + 1)
+    //     )
+    // }
+    // return Promise.reject("O número deve ser positivo")
+    res = (numero >=1 ? Promise.resolve((numero/2) * (numero + 1)) : Promise.reject('o número deve ser positivo'))
+    return res
 }
+
+calcular(10)
+.then(res => console.log(res))
+.catch(erro => console.log('erro' + erro))
+
+calcular(-2)
+.then(res => console.log(res))
+.catch(erro => console.log('erro' + erro))
